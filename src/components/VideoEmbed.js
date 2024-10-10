a1f5b7747caa5aedea2db60f4f7152665f6eab45
Liam Bailey <150624032+WidgetMasterLiam@users.noreply.github.com>
2024-10-10 19:42:00 -0400

    Update VideoEmbed.js

--- src/components/VideoEmbed.js
+++ src/components/VideoEmbed.js
@@ -9,7 +9,7 @@ const VideoEmbed = () => {
         <iframe
           width="100%"
           height="500px"
-          src="https://www.youtube.com/embed/oeHkibEAG50"
+          src="https://www.youtube.com/embed/GRCgHGci5AQ"
           title="The Ultimate Team Productivity Tool Overview"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
