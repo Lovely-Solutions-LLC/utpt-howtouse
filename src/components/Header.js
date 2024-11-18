8e2c6d901cebe9d140868d0d9270ae153fc1ce82
Liam Bailey <150624032+WidgetMasterLiam@users.noreply.github.com>
2024-11-18 06:55:46 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header-section" data-aos="fade-down">
-      <h1 className="main-header">The Ultimate Team Productivity Tool</h1>
+      <h1 className="main-header">Master Activity Logs</h1>
       <h2 className="sub-header">The Future of monday.com Productivity</h2>
     </header>
   );
